be304a8c57c1e60d80a5b6b742c6269065fd8b0b
mikhails85 <mikhails85@outlook.com>
2017-07-19 03:04:27 -0400

    Fix Build

--- src/web/app/app.module.ts
+++ src/web/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+     BrowserModule.withServerTransition({appId: 'web'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
