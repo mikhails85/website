3d574cbce2141f6de15d4a161e49db9531c3be2a
mikhails85 <mikhails85@outlook.com>
2017-08-04 11:53:48 -0400

    Server side rendering

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { HttpServer } from './server/http-server';
+class App {
+    
+   server:HttpServer; 
+   constructor() {  
+      this.server = new HttpServer();
+   }
+
+   public Run(){     
+     this.server.start(8800);
+   }
+}
+
+let app = new App();
+app.Run();
